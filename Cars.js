4ae33b1ff395ec8827544d29a6b642141418b832
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-04-25 11:55:47 +0530

    Add files via upload

---
+++ Cars.js
@@ -0,0 +1,62 @@
+const banner = document.getElementById('form', f1 = ()=> {
+    const set1 = document.addEventListener('mouseover')
+    {
+        alert("Input Error");
+    }
+
+    const set2 = document.getElementById('form', finput1 = ()=> {
+        <form action="formsubmit.co">
+            <label htmlFor="name">Customer Name:</label>
+            <input type="text" name="name" id="name" />
+            <label htmlFor="email">Customer Email:</label>
+            <input type="email" name="email" id="email" />
+            <label htmlFor="number">Customer Phone Number:</label>
+            <input type="number" name="number" id="number" />
+            <label htmlFor="message">Your Order:</label>
+            <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        </form>
+
+        document.getElementsByClassName('formset', finput2 = ()=> {
+           const s1 = document.getElementById('name');
+           const s2 = document.getElementById('email');
+           const s3 = document.getElementById('number');
+           const s4 = document.getElementById('message');
+           s1.style.background('transparent');
+           s2.style.background('transparent');
+           s3.style.background('transparent');
+           s4.style.background('transparent');
+        });
+    });
+    
+    const setinput = ()=> {
+        s1.addEventListener('hover',CSSRuleList);
+        s2.addEventListener('hover',CSSRuleList);
+        s3.addEventListener('hover',CSSRuleList);
+        s4.addEventListener('hover',CSSRuleList);
+    }
+});
+
+const tag1 = document.getElementById('sethere', func1 = () => {
+    if(s1.action) {
+        s2.document = satisfies;
+    }
+    if(s3.action && s4.action) {
+        s4.document = satisfies;
+    }
+});
+
+const tag2 = document.getElementById('sethere2', func2 = () => {
+    if(tag2.accessKey)
+    <p>Your form has been submitted</p>
+    const settag = document.getElementsByClassName('container-2', f = ()=> {
+        s1.action  = s1.style.background('darkblue');
+    });
+
+    let one = s2.finput1 = f = () =>{
+        if(one.getElementById('sethere')) undefined
+        else if(!one.getElementById('sethere'))
+        {
+            s1.satisfies = undefined
+        }
+    }
+});
